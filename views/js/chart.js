d09afe1a665bcbcf96222fd05e2c3d1c527d3783
khairiyanhidayat.ramadhan17@gmail.com <khairiyanhidayat.ramadhan17@gmail.com>
2023-01-30 13:48:30 +0700

    fix bug pada chart viewReport

--- views/js/chart.js
+++ views/js/chart.js
@@ -1,9 +1,31 @@
-let dataTime = [];
+let dataVisit = [];
 for (let i = 15; i > 1; i--) {
   var element = document.getElementsByName("totalVisit" + i)[0];
+  let valueTotalVisit = element.childNodes[0].nodeValue;
+  let valueInt = parseInt(valueTotalVisit);
+  dataVisit.push(valueInt);
+}
+
+let dataDay = [];
+let dataDate = [];
+let j = 13;
+for (let i = 15; i > 1; i--) {
+  var element = document.getElementsByName("date" + i)[0];
   let value = element.childNodes[0].nodeValue;
-  let valueInt = parseInt(value);
-  dataTime.push(valueInt);
+  let valueDayString = getDayName(value);
+  let valueDateString = getDate(value);
+  dataDate.push(valueDateString);
+  dataDay.push(valueDayString);
+}
+
+function getDayName(dateStr) {
+  var date = new Date(dateStr);
+  return date.toLocaleDateString("in-IN", { weekday: "long" });
+}
+
+function getDate(dateStr) {
+  var date = new Date(dateStr);
+  return date.getDate();
 }
 
 var options = {
@@ -14,11 +36,26 @@ var options = {
   series: [
     {
       name: "pengunjung",
-      data: [dataTime[13] + dataTime[6], dataTime[12] + dataTime[5], dataTime[11] + dataTime[4], dataTime[10] + dataTime[3], dataTime[9] + dataTime[2], dataTime[8] + dataTime[1], dataTime[7] + dataTime[0]],
+      data: [dataVisit[13], dataVisit[12], dataVisit[11], dataVisit[10], dataVisit[9], dataVisit[8], dataVisit[7], dataVisit[6], dataVisit[5], dataVisit[4], dataVisit[3], dataVisit[2], dataVisit[1], dataVisit[0]],
     },
   ],
   xaxis: {
-    categories: ["Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"],
+    categories: [
+      dataDay[13] + " " + dataDate[13],
+      dataDay[12] + " " + dataDate[12],
+      dataDay[11] + " " + dataDate[11],
+      dataDay[10] + " " + dataDate[10],
+      dataDay[9] + " " + dataDate[9],
+      dataDay[8] + " " + dataDate[8],
+      dataDay[7] + " " + dataDate[7],
+      dataDay[6] + " " + dataDate[6],
+      dataDay[5] + " " + dataDate[5],
+      dataDay[4] + " " + dataDate[4],
+      dataDay[3] + " " + dataDate[3],
+      dataDay[2] + " " + dataDate[2],
+      dataDay[1] + " " + dataDate[1],
+      dataDay[0] + " " + dataDate[0],
+    ],
   },
   yaxis: {
     title: {
