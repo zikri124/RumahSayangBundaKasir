f1842f49c9c8ae0194a56da62bc859914ee41d2a
khairiyanhidayat.ramadhan17@gmail.com <khairiyanhidayat.ramadhan17@gmail.com>
2023-02-03 15:16:10 +0700

    membuat tampilan mobile pada viewPriceSubmit

--- views/js/priceSubmit.js
+++ views/js/priceSubmit.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
     if (check) {
       $(wrapper).append(
         `<a href="/" class="">
-         <button class="btn btn-outline border-pink-500 w-[17.4rem] hover:bg-pink-500 hover:border-none">kembali ke dashboard </button>
+         <button class="btn btn-outline border-pink-500 md:w-[17.4rem] hover:bg-pink-500 hover:border-none">kembali ke dashboard </button>
        </a>`
       );
       check = false;
